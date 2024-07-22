bc7d571cc7fb31826c2c31d2c91947cecddd3ad0
thehxdev <hossein.khosravi.ce@gmail.com>
2024-07-23 03:20:14 +0330

    bump version to 1.0.0
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/thehxdev/403unlocker-go/tester"
 )
 
-const VERSION = "0.1.0"
+const VERSION = "1.0.0"
 
 var versionInfo = "403unlocker-go v" + VERSION + "\nhttps://github.com/thehxdev/403unlocker-go"
 
